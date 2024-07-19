7673e41fc6c71fcf90c21ed433456f5878a11073
farhank-py <farhan.kashif@arbisoft.com>
2024-07-19 20:32:29 +0500

    Improved Overall UI

--- blogapp/blogapp/settings.py
+++ blogapp/blogapp/settings.py
@@ -17,7 +17,9 @@ ALLOWED_HOSTS = ["testserver", "*"]
 
 STATIC_URL = "/static/"
 
-STATICFILES_DIRS = [ BASE_DIR / "static" ]
+STATICFILES_DIRS = [BASE_DIR / "static",
+                    BASE_DIR / 'blogapp/blog/static',
+                    ]
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
